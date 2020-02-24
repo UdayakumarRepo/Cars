3d60ccd4e3e94e70f34c05bfb2f4ebe1649a5a6c
uday <udayakumarl@deemsysinc.com>
2020-02-24 14:54:42 +0530

    first commit

---
+++ Cars/Cars/CarModel.swift
@@ -0,0 +1,23 @@
+//
+//  CarModel.swift
+//  Cars
+//
+//  Created by admin on 24/02/20.
+//  Copyright © 2020 deemsys. All rights reserved.
+//
+
+import Foundation
+
+
+public class CarModel{
+    
+    public static let shared = CarModel()
+    
+    private init() {
+    }
+    
+    public func whoAreYour() -> String {
+        return "I am an iOS Framwork.!"
+    }
+
+}
---
+++ Cars/Cars/Cars.h
@@ -0,0 +1,19 @@
+//
+//  Cars.h
+//  Cars
+//
+//  Created by admin on 24/02/20.
+//  Copyright © 2020 deemsys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cars.
+FOUNDATION_EXPORT double CarsVersionNumber;
+
+//! Project version string for Cars.
+FOUNDATION_EXPORT const unsigned char CarsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cars/PublicHeader.h>
+
+
